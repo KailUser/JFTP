1b2f2c41cd2e94db7436302d65c5e89b5fac796a
Colin <harebovmakar@yandex.ru>
2023-01-26 17:12:25 +0500

    New Update

--- files/ftp_server_local.py
+++ files/ftp_server_local.py
@@ -3,10 +3,10 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
-authorizer.add_user("user", "password", ".", perm="elradfmw")
+authorizer.add_user("root", "root", ".", perm="elradfmw")
 
 handler = FTPHandler
 handler.authorizer = authorizer
-
 server = FTPServer(("127.0.0.1", 21), handler)
 server.serve_forever()
+print(f'User: root Password: root')
