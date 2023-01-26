1cdc5415aec93ed87b5f5a670b18e78a41f2b01b
Colin <harebovmakar@yandex.ru>
2023-01-26 16:27:02 +0500

    New Update!

--- files/main.py
+++ files/main.py
@@ -1,18 +1,27 @@
 import colorama
 from colorama import *
 import core as core
+import os
 
 
 colorama.init(autoreset=True)
+print(Fore.LIGHTGREEN_EX + """
+░░░░░██╗███████╗████████╗██████╗░
+░░░░░██║██╔════╝╚══██╔══╝██╔══██╗
+░░░░░██║█████╗░░░░░██║░░░██████╔╝
+██╗░░██║██╔══╝░░░░░██║░░░██╔═══╝░
+╚█████╔╝██║░░░░░░░░██║░░░██║░░░░░
+░╚════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░░░░░
 
-print("1. Connect to SSH")
-print("2. Connect to FTP")
+1. Connect to FTP Server
+2. Exit""")
 
 selection = int(input("Select: "))
 
 if selection == 1:
-    print(core.connect_ssh())
-elif selection == 2:
+    os.system('clear')
     print(core.ftp())
+elif selection == 2:
+    print("Exiting the program.")
 else:
     print("Invalid select. Please try again.")
