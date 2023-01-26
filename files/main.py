5c9b0082a1a7e4c7ca2248084f68456473d9fb68
Colin <harebovmakar@yandex.ru>
2023-01-26 16:31:16 +0500

    New Update!

--- files/main.py
+++ files/main.py
@@ -14,14 +14,18 @@ print(Fore.LIGHTGREEN_EX + """
 ░╚════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░░░░░
 
 1. Connect to FTP Server
-2. Exit""")
+2. Local Ftp Server
+3. Exit""")
 
 selection = int(input("Select: "))
 
 if selection == 1:
-    os.system('clear')
+    os.system('cls')
     print(core.ftp())
 elif selection == 2:
+    os.system('cls')
+    os.system(f'python -b %cd%//files//ftp_server_local.py')
+elif selection == 3:
     print("Exiting the program.")
 else:
     print("Invalid select. Please try again.")
